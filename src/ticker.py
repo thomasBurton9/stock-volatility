Fixed standard deviation and annualized volatility logic

src/ticker.py
@@ -32,7 +32,7 @@ def get_ticker_volatility(ticker: str, time_period: str, volatility_time_period:
     # Implement - for each window from just [0] to [len(data)-window, len(data)]
     avg_volatility = np.zeros_like(avg_log_return)
 
-    for i in range(1, len(avg_log_return)+1):
+    for i in range(2, len(avg_log_return)+1):
         initial_index = i - volatility_time_period
         initial_index = max(initial_index, 0)
         standard_deviation = get_std(avg_log_return[initial_index: i])

src/volatility.py
@@ -8,7 +8,7 @@ def get_alltime_logarithmic_return(data):
     return np.log(close_prices2 / close_prices)
 
 def get_std(logarithmic_returns):
-    return np.std(logarithmic_returns)
+    return np.std(logarithmic_returns, ddof=1)
 
 def get_annualized_volatility(standard_deviation, days: int):
-    return standard_deviation * math.sqrt((days / 365) * 252)
+    return standard_deviation * math.sqrt(252)